062acba49d3ab295bd7dfb2bfc719dae6eef0fa4
KradasA4 <wichai.sw19@gmail.com>
2019-08-24 17:59:42 +0700

    Create Exercise4_Wichai_S.py
    First Commit
    - define cariable then print

---
+++ Exercise4_Wichai_S.py
@@ -0,0 +1,11 @@
+FEng = 84.0               #คะแนน Foundation English
+GBusi = 52.2              #คะแนน General Business
+IntroComSys = 60.0        #คะแนน Introduction to Computer System
+ComProg = 76.4     #คะแนน Computer Programming
+
+
+print("--- Your Score ---")
+print("Foundation English :", FEng)
+print("General Business :", GBusi)
+print("Introduction to Computer System", IntroComSys)
+print("Computer Programming", ComProg)
