7eae28d63df0a362a5e091dd80070c3652fc7212
KradasA4 <wichai.sw19@gmail.com>
2019-09-08 23:10:24 +0700

    Create Exercise11_WIchai_S.py
    the easiest way to solve the problem imo, not sure about the comprehensibility though.

---
+++ Exercise11_WIchai_S.py
@@ -0,0 +1,11 @@
+round = int(input("stories of the pyramid: "))
+for x in range(round):
+    space = " "*(round-(x+1))
+    print(space+(("*"*x)+("*"*(x+1))))
+
+
+
+#round = int(input("stories of the pyramid: "))
+#for x in range(round):
+#       print((" "*(round-(x+1)))+("*"*x)+("*"*(x+1)))
+
