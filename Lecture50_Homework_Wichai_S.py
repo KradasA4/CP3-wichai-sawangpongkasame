6c4ab54acd980cf0d8d8e2150d0e513102c9482e
KradasA4 <wichai.sw19@gmail.com>
2019-09-12 22:36:29 +0700

    Create Lecture50_Homework_Wichai_S.py
    Python homework: functions for  add, substract, multiply, divide numbers

---
+++ Lecture50_Homework_Wichai_S.py
@@ -0,0 +1,14 @@
+def addNumber(x,y):             #plus
+    print(x+y)
+def subtractNumber(x,y):        #minus
+    print(x-y)
+def multiplyNumber(x,y):        #multiply
+    print(x*y)
+def divideNumber(x,y):          #divide
+    print(int(x/y))
+
+
+addNumber(35,5)
+subtractNumber(35,5)
+multiplyNumber(35,5)
+divideNumber(35,5)
